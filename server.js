a5e6ca1fc1e742274fb4f8f9eff2e8712ee3c14c
Vinette Gutierrez <vinette.gutierrez@gmail.com>
2021-08-27 14:09:37 -0700

    route didn't work, removing code

--- server.js
+++ server.js
@@ -33,9 +33,6 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', function (req, res) {
-  res.json(path.join(__dirname, 'public/index.html'));
-});
 
 app.use(routes);
 
